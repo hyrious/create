8681e61a3479b13a3acf1f6ea730dc878443d389
hyrious <hyrious@outlook.com>
2026-05-19 10:15:02 +0800

    refactor: support bun and refactor options

--- cli.js
+++ cli.js
@@ -23,7 +23,7 @@ import { sortJSON } from '@hyrious/sort-package-json'
 // otherwise:
 //    src/index.ts
 //    src/cli.ts          // if '--cli'
-//    tsconfig.json       // apply '@hyrious/configs/tsconfig.casual.json'
+//    tsconfig.json       // apply '@hyrious/configs/tsconfig.{casual,node}.json'
 //    pkg.cli = dist/cli.js
 //    pkg.files = [src, dist]
 //
@@ -31,22 +31,23 @@ import { sortJSON } from '@hyrious/sort-package-json'
 //    main.{js,ts}        // '.js' if '--js' else '.ts'
 //    index.html
 //
-// If '--public', add:    // oidc is always enabled
+// If '--public[=github]', add:
 //    .github/workflows/npm-publish.yml
+//
+// Run '{npm,pnpm,bun} install' if '-i'
 //
 sade('@hyrious/create', true)
   .version(JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8')).version)
-  .option('-i, --install', 'Run `npm install` afterwards', false)
-  .option('--mirror', 'Secretly use npmmirror in `npm install`, implies --install', false)
+  .option('-i, --install', 'Run `npm install` afterwards, use mirror with -i=mirror', null)
   .option('--js', 'Use JavaScript', false)
-  .option('--cli', 'Add CLI entry point', false)
-  .option('--pnpm', 'Use pnpm instead of npm', false)
-  .option('--pnpm-better-defaults', 'Adopt @pnpm/plugin-better-defaults, implies --pnpm', false)
+  .option('--node', 'Use tsconfig.node.json', false)
+  .option('--cli', 'Add CLI entry point, implies --node', false)
+  .option('--pnpm', 'Use pnpm with @pnpm/plugin-better-defaults', false)
+  .option('--bun', 'Use bun with --cli and --install', false)
   .option('--vite', 'Install Vite', false)
   .option('--dual', 'Use ESM + CJS', false)
   .option('--author', 'Set the "author" field', 'hyrious <hyrious@outlook.com>')
-  .option('--public', 'Add workflows', false)
-  .option('--oidc', 'https://docs.npmjs.com/trusted-publishers')
+  .option('--public', 'Add publish workflows using OIDC or GitHub registry with --public=github', null)
   .option('--corepack', 'Use corepack, only work with --pnpm', false)
   .describe('Create a new project.')
   .action(async function hyrious_create_package(opts) {
@@ -59,12 +60,12 @@ sade('@hyrious/create', true)
 
     const win = process.platform === 'win32'
 
-    if (!opts.npm && opts['pnpm-better-defaults']) opts.pnpm = true
+    opts.mirror = opts.install === 'mirror'
+    opts.install = opts.install === true || opts.mirror
+    opts.github = opts.public === 'github'
+    opts.public = opts.public === true || opts.github
+    if (opts.cli) opts.node = true
     if (!opts.npm) opts.npm = !opts.pnpm
-
-    if (opts.mirror) opts.install = true
-    if (opts.oidc) opts.public = true
-    opts.oidc = true
 
     const writeFile = (name, data) => {
       console.log('create', name)
@@ -123,7 +124,7 @@ sade('@hyrious/create', true)
       if (opts.cli) {
         writeFile('src/cli.ts', 'console.log(1)\n\n// "#!/usr/bin/env node" will make this file be detected as js by github.\n')
         pkg.bin = 'dist/cli.js'
-        pkg.devDependencies['@types/node'] = '*'
+        pkg.devDependencies[opts.bun ? '@types/bun' : '@types/node'] = '*'
       }
       pkg.main = 'dist/index.js'
       if (opts.dual) {
@@ -134,7 +135,7 @@ sade('@hyrious/create', true)
       pkg.devDependencies['@hyrious/configs'] = '*'
       writeFile('tsconfig.json', `{
   "include": ["src"],
-  "extends": "@hyrious/configs/tsconfig.casual.json"
+  "extends": "@hyrious/configs/tsconfig.${opts.node ? 'node' : 'casual'}.json"
 }\n`)
     }
 
@@ -157,6 +158,7 @@ sade('@hyrious/create', true)
 
     if (opts.public) {
       fs.mkdirSync('.github/workflows', { recursive: true })
+      const registry = opts.github ? 'https://npm.pkg.github.com' : 'https://registry.npmjs.org'
       writeFile('.github/workflows/npm-publish.yml', `name: Node.js Package
 
 on:
@@ -170,7 +172,7 @@ jobs:
     runs-on: ubuntu-latest
     permissions:
       contents: read
-      id-token: write
+      ${opts.github ? 'packages: write' : 'id-token: write'}
     concurrency:
       group: \${{ github.workflow }}-\${{ github.ref }}
       cancel-in-progress: true
@@ -182,17 +184,18 @@ jobs:
       - uses: actions/setup-node@v6
         with:
           node-version: "lts/*"
-          registry-url: "https://registry.npmjs.org"
+          registry-url: "${registry}"${opts.github ? `
+          scope: "@${userLower}"` : ''}
           package-manager-cache: false
       - run: |${opts.npm ? `
           npm ci
           npm run build` : `
           pnpm install
           pnpm build`}${opts.npm ? `
-      - run: npm publish --access public` : `
-      - run: pnpm publish --access public --no-git-checks`}${opts.oidc ? '' : `
+      - run: npm publish${opts.github ? '' : ' --access public'}` : `
+      - run: pnpm publish${opts.github ? '' : ' --access public'} --no-git-checks`}${opts.github ? `
         env:
-          NODE_AUTH_TOKEN: \${{ secrets.NPM_TOKEN }}`}\n`)
+          NODE_AUTH_TOKEN: \${{ secrets.GITHUB_TOKEN }}` : ''}\n`)
     }
 
     writeFile('README.md', `# @${userLower}/${name}
@@ -226,46 +229,50 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 `)
 
-    if (Object.keys(pkg.devDependencies).length > 0 || pkg.packageManager) {
-      const packages = Object.keys(pkg.devDependencies)
-      if (pkg.packageManager) packages.push('pnpm')
-      console.info('resolving', ...packages)
+    const names = Object.keys(pkg.devDependencies)
+    const plugins = opts.pnpm ? ['@pnpm/plugin-better-defaults', '@pnpm/plugin-trusted-deps'] : []
+    if (pkg.packageManager) names.push('pnpm')
+    names.push(...plugins)
+    let versions = { __proto__: null }
+    if (names.length > 0) {
+      console.info('resolving', ...names)
 
       // https://github.com/antfu/fast-npm-meta#-resolve-multiple-packages
-      const latestVersions = async (names) => {
+      const latest = async (names) => {
         const payload = encodeURIComponent(names.join('+'))
         const response = await fetch('https://npm.antfu.dev/' + payload)
         const data = await response.json()
         if (response.ok) {
           if (Array.isArray(data)) {
-            return data.reduce((deps, a) => { deps[a.name] = `^${a.version}`; return deps }, {})
+            return data.reduce((deps, a) => { deps[a.name] = a.version; return deps }, { __proto__: null })
           } else {
-            return { [data.name]: `^${data.version}` }
+            return { __proto__: null, [data.name]: data.version }
           }
         } else {
           console.warn(data && data.message || data || 'failed to fetch npm.antfu.dev')
-          return latestVersionsFallback(names)
+          return fallback(names)
         }
       }
 
       // https://www.jsdelivr.com/docs/data.jsdelivr.com#get-/v1/packages/npm/-package-/resolved
-      const latestVersionsFallback = async (names) => {
-        const tasks = [], deps = {}
+      const fallback = async (names) => {
+        const tasks = [], deps = { __proto__: null }
         for (const name of names) {
           tasks.push(fetch(`https://data.jsdelivr.com/v1/packages/npm/${name}/resolved`)
             .then(r => r.json())
-            .then(a => { deps[a.name] = `^${a.version}` }))
+            .then(a => { deps[a.name] = a.version }))
         }
         await Promise.allSettled(tasks)
         return deps
       }
 
-      const versions = await latestVersions(packages)
+      versions = await latest(names)
       if (pkg.packageManager && versions.pnpm) {
-        pkg.packageManager = `pnpm@${versions.pnpm.slice(1)}`
-        delete versions.pnpm
+        pkg.packageManager = `pnpm@${versions.pnpm}`
       }
-      Object.assign(pkg.devDependencies, versions)
+      for (const name in pkg.devDependencies) {
+        if (versions[name]) pkg.devDependencies[name] = `^${versions[name]}`
+      }
 
       // Check if done right
       for (const name in pkg.devDependencies) {
@@ -277,41 +284,38 @@ SOFTWARE.
 
     writeFile('package.json', sortJSON(pkg))
 
-    if (opts['pnpm-better-defaults']) {
-      const registry = opts.mirror ? 'https://registry.npmmirror.com' : 'https://registry.npmjs.org'
-      const hashes = { '@pnpm/plugin-better-defaults': '', '@pnpm/plugin-trusted-deps': '' }
-      for (const name in hashes) {
-        const data = await fetch(`${registry}/${name}`).then(r => r.json())
-        const version = data['dist-tags'].latest
-        const meta = data.versions[version]
-        hashes[name] = version + '+' + meta.dist.integrity
-      }
+    if (opts.pnpm) {
       writeFile('pnpm-workspace.yaml', `
 configDependencies:
-  '@pnpm/plugin-better-defaults': ${hashes['@pnpm/plugin-better-defaults']}
-  '@pnpm/plugin-trusted-deps': ${hashes['@pnpm/plugin-trusted-deps']}
+  '@pnpm/plugin-better-defaults': ${versions[plugins[0]]}
+  '@pnpm/plugin-trusted-deps': ${versions[plugins[1]]}
 `.trimStart())
     }
 
     if (opts.install) {
-      let bin = opts.npm ? 'npm' : 'pnpm'
-      if (win) bin += '.cmd';
+      let bin = opts.bun ? 'bun' : opts.npm ? 'npm' : 'pnpm'
+      if (win && !opts.bun) bin += '.cmd';
       let env = { ...process.env }
       if (opts.mirror) env.NPM_CONFIG_REGISTRY = 'https://registry.npmmirror.com';
       process.exitCode = cp.spawnSync(bin, ['install'], { env, stdio: 'inherit', shell: win }).status
       if (opts.mirror) {
         console.info('hint: NPM_CONFIG_REGISTRY=https://registry.npmmirror.com')
-        if (opts.npm) {
-          const lockfile = 'package-lock.json'
+        if (opts.npm || opts.bun) {
+          const lockfile = opts.bun ? 'bun.lock' : 'package-lock.json'
           let contents = fs.readFileSync(lockfile, 'utf8')
-          contents = contents.replaceAll(env.NPM_CONFIG_REGISTRY, 'https://registry.npmjs.org')
+          if (opts.bun) {
+            const mirror = env.NPM_CONFIG_REGISTRY.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
+            contents = contents.replace(new RegExp(`"${mirror}[^"]+"`, 'g'), '""')
+          } else {
+            contents = contents.replaceAll(env.NPM_CONFIG_REGISTRY, 'https://registry.npmjs.org')
+          }
           console.log('update', lockfile)
           fs.writeFileSync(lockfile, contents)
         }
       }
     }
 
-    const pm = opts.npm ? 'npm i' : 'pnpm add'
+    const pm = opts.bun ? 'bun add' : opts.npm ? 'npm i' : 'pnpm add'
 
     console.log()
     console.log('next steps:')
